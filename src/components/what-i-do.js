5efbe7de363a49fbd29b7b1909be3711c26f6f4f
ariel-gova <ariel.pasilang@go-va.com.au>
2022-01-27 23:37:36 +0800

    Test Update

--- src/components/what-i-do.js
+++ src/components/what-i-do.js
@@ -5,9 +5,14 @@ const WhatIDo = () => {
     <div>
     <h3 className="h3-black">What I Do</h3>
     <h4 className="h4-primary">Front-End Development</h4>
-    <p>Hello, Universe. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. 
-      The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. 
-      The quick brown fox jumps over the lazy dog.</p>
+      <ul>
+        <li>Convert Figma/PSD Designs to Responsive HTML5/CSS3, React and Gatsby Templates</li>
+        <li>Mobile First Approach</li>
+        <li>Graphql and REST API Integration to React and Gatsby Frameworks</li>
+        <li>Knowledgeable in using Headless CMS such as Strapi, Sanity and NetlifyCMS</li>
+        <li>Proficient in using Jquery and Ajax </li>
+      </ul>
+    <p></p>
       <h4 className="h4-primary">Back-End Development</h4>
     <p>Hello, Universe. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. 
       The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. 
